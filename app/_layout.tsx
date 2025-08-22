5c279b6b5c40a6ca07b67ce907e81fa8b8034efc
Shivang Sharma <sharmashivang1999@gmail.com>
2025-08-22 17:45:49 +0530

    feat: query client created and added in the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,46 @@
+import { tokenManager } from "@/services/api";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: (failureCount, error) => {
+        if (error && typeof error === "object" && "statusCode" in error) {
+          const apiError = error as { statusCode: number };
+
+          if (apiError.statusCode >= 400 && apiError.statusCode < 500)
+            return false;
+        }
+        return failureCount < 3;
+      },
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
 
 export default function RootLayout() {
+  useEffect(() => {
+    const initialiseApp = async () => {
+      try {
+        await tokenManager.initialize();
+        console.log("✅ API service initialized successfully");
+      } catch (error) {
+        console.error("❌ Failed to initialize API service: " + error);
+      }
+    };
+
+    initialiseApp();
+  }, []);
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name="splash" options={{ headerShown: false }} />
         <Stack.Screen name="register" options={{ headerShown: false }} />
@@ -15,6 +51,6 @@ export default function RootLayout() {
         <Stack.Screen name="otp-auth" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </QueryClientProvider>
   );
 }
